4f911ad2bfe337b3b01ff09b0719db94973ea91f
admin <admin@SERVER0>
2024-06-15 15:05:38 +0330

    رفع اشکال

--- TiketKarbord/Controllers/Unit/UnitPublic.cs
+++ TiketKarbord/Controllers/Unit/UnitPublic.cs
@@ -5,7 +5,7 @@ namespace TiketKarbord.Controllers.Unit
     public class UnitPublic
     {
        public static string titleVer = "ورژن تست";
-       public static string titleVerNumber = "12";
+       public static string titleVerNumber = "13";
 
       //public static string titleVer = "ورژن"; 
       //public static string titleVerNumber = "1001";
